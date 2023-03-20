3521b3ee434532f2991e333d6a96786b0614b661
Nikita Pisarenko <nikitapisarenko1995@gmail.com>
2023-03-20 18:53:16 +0300

    init config files

---
+++ config-client/src/main/java/com/pisarenko/ConfigClient.java
@@ -0,0 +1,16 @@
+package com.pisarenko;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);}
+}
+
+
